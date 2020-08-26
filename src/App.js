3edd265d718958877e496b4a1818618d12bef177
MUHAMMAD HAMZA KHAN <mhk420520@gmail.com>
2020-08-26 15:59:46 +0500

    Expense App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Header} from './Components/header';
+import {Balance} from './Components/Balance';
+import {IncomeExpenses} from './Components/incomeExpenses';
+import {TransactionList} from './Components/TransactionList';
+import {AddTransaction} from './Components/AddTransaction';
+import {GlobalProvider } from './Context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+     <Header />
+     <div className="container">
+     <Balance />
+     <IncomeExpenses />
+     <TransactionList />
+     <AddTransaction />
+
+     </div>
+     
+     </GlobalProvider>
   );
 }
 
